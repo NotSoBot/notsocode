3bb9f8903003d77c1b7a616b5d1c62059b8d40fe
Dan <31395415+cakedan@users.noreply.github.com>
2025-02-18 17:15:16 -0800

    Add prolog, add pydub to python 3+, add dkjson to lua, add `bc` globally

---
+++ notsocode/dockerfiles/prolog/9.3.20/test.pl
@@ -0,0 +1,2 @@
+main :- write('Hello, World!'), nl.
+:- initialization(main).
--- notsocode/utilities/constants.py
+++ notsocode/utilities/constants.py
@@ -65,6 +65,7 @@ class Languages(Enumerable):
     NASM_64 = ('nasm64', ('2.16.1',), None, 'asm')
     NODE = ('node', ('19.7.0', '21.1.0'), '21.1.0', 'js')
     PHP = ('php', ('8.2.3',), None, 'php')
+    PROLOG = ('prolog', ('9.3.20',), None, 'pl')
     PYTHON = ('python', ('2.7.18', '3.9.16', '3.11.6', '3.12.0'), '3.11.6', 'py')
     PYTHON_2 = ('python', ('2.7.18',), None, 'py')
     RUBY = ('ruby', ('3.2.1',), None, 'rb')
